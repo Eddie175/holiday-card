e7fcada12a65a73181aa267f78a9a5c67084a97d
Eddie Gutierrez <eddygutierrez175@gmail.com>
2021-03-15 02:00:17 -0700

    Changed Layout
    I wasn't happy with the way that the days were displayed so I changed it and added a month counter ! Super happy with how this one came out

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // Javascript File
 
+const getDaysInMonth = (month, year) => {
+  month = month + 1;
+  return new Date(year, month, 0).getDate();
+};
+
 const changeCountdown = () => {
   let date = new Date();
-  if (date.getFullYear() === 2021) {
-    clear
-    return document.getElementById("message").innerHTML = "We made it out alive! Yay!";
+  if (date.getFullYear() === 2022) {
+    return (document.getElementById("message").innerHTML =
+      "We made it out alive! Yay!");
   }
-  let days = document.getElementById("days").innerHTML = 366 % Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-  let hours = document.getElementById("hours").innerHTML = 23 - date.getHours();
-  let minutes = document.getElementById("minutes").innerHTML = 59 - date.getMinutes();
-  let seconds = document.getElementById("seconds").innerHTML = 59 - date.getSeconds();
-} 
+  let days = (document.getElementById("days").innerHTML =
+    getDaysInMonth(date.getMonth(), date.getFullYear()) - date.getDate());
+  let months = (document.getElementById("months").innerHTML =
+    11 - date.getMonth());
+  let hours = (document.getElementById("hours").innerHTML =
+    23 - date.getHours());
+  let minutes = (document.getElementById("minutes").innerHTML =
+    59 - date.getMinutes());
+  let seconds = (document.getElementById("seconds").innerHTML =
+    59 - date.getSeconds());
+};
 
 changeCountdown();
 
 setInterval(() => {
-  console.log("hello");
   changeCountdown();
 }, 1000);
